8418d23de7ddee4abada13165345be66268aaa26
Josef Pötzl <josef.poetzl@joposol.at>
2024-04-14 22:15:25 +0200

    + add WPF UserControl

--- source/AccessCodeLib.Common.VbeUserControlHost/IVbeUserControlHost.cs
+++ source/AccessCodeLib.Common.VbeUserControlHost/IVbeUserControlHost.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
+
 
 namespace AccessCodeLib.Common.VBIDETools
 {
     [ComVisible(true)]
-    [Guid("0EEEA3E7-68D6-49BA-8536-572E69CCCEF0")]
-    public interface IVbeUserControlHost
+    [Guid("007610E2-E9A0-498E-867E-36AA8BC4D71C")]
+    public interface IVbeUserControlHost : IVbeWindowsFormsUserControlHost, IVbeWPFUserControlHost
     {
-        void HostUserControl(UserControl UserControlToHost);
     }
+
+    [ComVisible(true)]
+    [Guid("326CB39E-C621-41F6-833D-BACE54D26763")]
+    public interface IVbeWindowsFormsUserControlHost
+    {
+        void HostUserControl(System.Windows.Forms.UserControl UserControlToHost);
+    }
+
+    [ComVisible(true)]
+    [Guid("A5014EE3-82C7-4790-B12F-4EE2C95092D2")]
+    public interface IVbeWPFUserControlHost
+    {
+        void HostUserControl(System.Windows.Controls.UserControl UserControlToHost);
+    }
+
+
 }
